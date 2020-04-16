074d6fe41222d7e1c8f21ba8e4d4020b2cd27d6f
Ryan <thewebcoderz@gmail.com>
2020-04-15 23:53:03 -0400

    added hmtl template files and added them to the views file

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('home')
+    return render(request, 'accounts/dashboard.html')
 
 def products(request):
-    return HttpResponse('products')
+    return HttpResponse('accounts/products.html')
 
 def customer(request):
-    return HttpResponse('customer')
+    return HttpResponse('accounts/customer.html')
